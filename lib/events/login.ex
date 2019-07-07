e262877e1e1148c2d8f5ce6e8d4d08cf1fa35eb6
Namdak Tonpa <maxim@synrc.com>
2019-07-07 22:04:12 +0300

    cookie sample

--- lib/events/login.ex
+++ lib/events/login.ex
@@ -5,11 +5,16 @@ defmodule PLM.Login do
 
   def event(:init) do
     NITRO.clear(:stand)
+    NITRO.cookie 'hello', 'world'
+    NITRO.wire(api(name: 'getCookies', delegate: PLM.Login))
     mod = PLM.Forms.Pass
     NITRO.insert_bottom(:stand, FORM.new(mod.new(mod, mod.id()), mod.id()))
   end
 
   def event({:Next, _}), do: NITRO.redirect("plm.htm")
+  def event({:SMS, _}), do: NITRO.wire("document.getCookies('sample string');")
   def event({:Close, _}), do: NITRO.redirect("index.html")
-  def event(any), do: IO.inspect(any)
+  def event(any), do: IO.inspect any
+
+  def api_event(_,_,_), do: IO.inspect "API EVENT~n"
 end
