a5f8a69fce891ddb89f2e7b6c6723d4849838b89
Namdak Tonpa <maxim@synrc.com>
2019-07-11 20:41:39 +0300

    wip

--- lib/vector/product.ex
+++ lib/vector/product.ex
@@ -9,10 +9,19 @@ defmodule PLM.Rows.Product do
     do: "PLM product."
 
   def id(), do: ERP."Product"()
+  def mul({a,b},{c,d}), do: {a+c,b*d}
+
+  def sum(feed) do
+     {_,b} = :lists.foldl(fn (ERP."Payment"(volume: v, price: p),{x,y}) ->
+        {_,y1}= mul(v,p)
+        {x,y+y1} end, {0, 0}, :kvs.all feed)
+     b
+  end
 
   def new(name, prod) do
     code = ERP."Product"(prod, :code) |> NITRO.to_binary()
-
+    income = (("/plm/" <> code <> "/income") |> :erlang.binary_to_list |> sum)
+    outcome = (("/plm/" <> code <> "/outcome") |> :erlang.binary_to_list |> sum)
     panel(
       id: FORM.atom([:tr, name]),
       class: :td,
@@ -23,11 +32,13 @@ defmodule PLM.Rows.Product do
         ),
         panel(
           class: :column6,
-          body: "EBITDA:<br>ROI:<br>Revenue:"
+          body: "Total Income: " <> NITRO.to_binary(income) <> "<br>" <>
+                "Gross Profit: " <> NITRO.to_binary(income-outcome) <> "<br>"
         ),
         panel(
           class: :column20,
-          body: "Maxim"
+          body: :string.join(:lists.map(fn (ERP."Person"(cn: id, hours: h)) -> id ++  ' (' ++ :erlang.integer_to_list(h) ++')'
+      end, :kvs.all '/plm/'++ :erlang.binary_to_list(code) ++ '/staff'), ',')
         ),
         panel(
           class: :column30,
