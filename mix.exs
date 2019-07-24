15a4b0eb2c792797fe5b61cc13bbfbd4112f5dbe
Namdak Tonpa <maxim@synrc.com>
2019-07-24 16:18:31 +0300

    o7

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule PLM.Mixfile do
       licenses: ["ISC"],
       maintainers: ["Namdak Tonpa"],
       name: :plm,
-      links: %{"GitHub" => "https://github.com/enterprizing/plm"}
+      links: %{"GitHub" => "https://github.com/o7/plm"}
     ]
   end
 
